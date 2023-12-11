f5b0ffb6390b7d6fc121435ae0d8fafd748d371a
Khushboo Agarwal <kushbooagarwal3584@gmail.com>
2023-12-11 19:42:12 +0530

    csv file added

--- database.py
+++ database.py
@@ -1,13 +1,22 @@
-import pandas as pd
-import glob
-import time
 import duckdb
+import json
 
-conn = duckdb.connect("webscrapedb.db")
-cursor = conn.cursor()
-#cursor.execute("DROP TABLE delegate_info")
+#CONNECT TO DATABASE
+conn = duckdb.connect("webscrape.db")
+cursor = conn.cursor()#
+cursor.execute("describe TABLE delegate_info")
+resu= cursor.fetchall()
 
-# cursor.execute('SELECT * FROM delegate_info')
+#SHOW CONTENT OF DELEGATE_INFO TABLE
 cursor.execute("SELECT * FROM delegate_info")
 result = cursor.fetchall()
-print(result)
+
+#CONVERT INTO JSON
+columns = [desc[0] for desc in cursor.description]
+data = [dict(zip(columns, row)) for row in result]
+json_data = json.dumps(data, indent=2)
+print(json_data)
+
+#EXPORT TO CSV
+csv_file_path = 'output1.csv'
+cursor.execute("COPY (SELECT * FROM delegate_info) TO '{}' WITH HEADER".format(csv_file_path))
--- index.py
+++ index.py
@@ -6,19 +6,20 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import StaleElementReferenceException
 import time
 import duckdb
-
+import subprocess
 
 website = 'https://ocimpact.com/delegate-roster/'
 driver = webdriver.Chrome()
 driver.get(website)
-# try:
-#     close_button = driver.find_element(By.XPATH, '//button[@title="Close"]')
-#     close_button.click()
-# except Exception as e:
-#     print(str(e))
+
+try:    
+    close_button = driver.find_element(By.XPATH, '//button[@title="Close"]')
+    close_button.click()
+except Exception as e:
+    pass
 
 #Connection to Database
-con = duckdb.connect(database='webscrapedb.db', read_only=False)
+con = duckdb.connect(database='webscrape.db', read_only=False)
 cursor = con.cursor()
 cursor.execute('CREATE TABLE IF NOT EXISTS delegate_info (Name VARCHAR, Job_Title VARCHAR, Organization VARCHAR, What_are_you_working_on_in_the_field_of_social_and_economic_justice VARCHAR, What_do_you_need_from_the_Delegate_community_to_move_this_work_forward VARCHAR, What_are_you_able_and_willing_to_contribute_to_the_Delegate_community VARCHAR)')
 
@@ -38,21 +39,24 @@ try:
             driver.get(href_attribute)
             WebDriverWait(driver, 200).until(EC.presence_of_element_located((By.XPATH, '//h2')))
             time.sleep(2)
-            
+
+            #SCRAPE CONTENT
             name = driver.find_element(By.XPATH, '//h2[@class="style__Name-cmp__sc-1s7e137-1 jhjTCw"]').text
             job_title = driver.find_element(By.XPATH, "//h4[@class='style__Job-cmp__sc-1s7e137-2 dzeTcv']").text
             organization = driver.find_element(By.XPATH, '//h3[@class="style__Organization-cmp__sc-1s7e137-3 cVzOUy"]').text
             questions = driver.find_elements(By.XPATH, "//div[@class='expandable-content__ExpandableWrapper-ui__sc-1k0xztj-0 kutcyn']")
             answers = []
             for ques in questions:
-                ques.find_element(By.XPATH, '//div[@class="expandable-content__ChildrenWrapper-ui__sc-1k0xztj-3 dCtHth"]')
-                answers.append(ques.text)
+                if(ques.find_element(By.XPATH, '//div[@class="expandable-content__ChildrenWrapper-ui__sc-1k0xztj-3 dCtHth"]')):
+                    answers.append(ques.text)
+                else:
+                    answers.append("No answer found")
+                    
                 
             size = len(answers)
-            print(size)
             while(len(answers) < 3):
-               answers.append("Not Answered") #missing value
-            print(answers)
+               answers.append("No answer found") #missing value
+
             # Insert data into DuckDB table
             cursor.execute("INSERT INTO delegate_info VALUES (?, ?, ?, ?, ?, ?)", (name, job_title, organization, answers[0], answers[1], answers[2]))
             driver.implicitly_wait(10)
@@ -65,11 +69,9 @@ try:
 finally:
     # Close the web driver
     driver.quit()
-    con.close()
 
-# Retrieve data from DuckDB table
-cursor.execute('SELECT * FROM delegate_info')
-result = cursor.fetchall()
-print(result)
+con.close()
+# file_to_run = 'database.py'
+# subprocess.run(['python', file_to_run])
 
 
